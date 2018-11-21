eed45924618a591838130b0bb0d6405024ea9ca0
张红军 <zhanghongjun228@163.com>
2018-11-21 22:16:53 +0800

    添加fax项目

---
+++ app/src/main/java/com/fax/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fax;
+
+import android.support.v7.app.AppCompatActivity;ls
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
